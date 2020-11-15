3c0078aa588ccba3de85a5a3a416bb3be383d9c9
SaharOladi <sahar.olady@gmail.com>
2020-11-15 09:42:49 +0330

    Add MusicPlayerTabActivity, Add AlbumFragment and PlayListFragment and SingerFragment. Add models.

---
+++ app/src/main/java/com/example/musicplayer/activity/MusicPlayerTabActivity.java
@@ -0,0 +1,124 @@
+package com.example.musicplayer.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.musicplayer.R;
+import com.example.musicplayer.fragment.AlbumFragment;
+import com.example.musicplayer.fragment.PlayListFragment;
+import com.example.musicplayer.fragment.SingerFragment;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicPlayerTabActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager2 mViewPager;
+    PageAdapter mPageAdapter;
+
+
+
+    public static Intent newIntent(Context context) {
+        Intent intent = new Intent(context, MusicPlayerTabActivity.class);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+        initView();
+    }
+
+
+    private void findViews() {
+        mViewPager = findViewById(R.id.view_pager);
+        mTabLayout = findViewById(R.id.tab_layout);
+    }
+
+    private void initView() {
+        mPageAdapter = new PageAdapter(this);
+        mViewPager.setAdapter(mPageAdapter);
+
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator
+                (mTabLayout, mViewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        switch (position) {
+                            case 0: {
+                                tab.setText("SONGS");
+                                break;
+                            }
+                            case 1: {
+                                tab.setText("ALBUM");
+                                break;
+                            }
+                            case 2: {
+                                tab.setText("ARTIST");
+                                break;
+                            }
+                        }
+                    }
+                });
+
+        tabLayoutMediator.attach();
+    }
+
+
+
+    private class PageAdapter extends FragmentStateAdapter {
+
+        public PageAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    PlayListFragment playListFragment = PlayListFragment.newInstance();
+                    return playListFragment;
+                case 1:
+                    AlbumFragment albumFragment = AlbumFragment.newInstance();
+                    return albumFragment;
+                case 2:
+                    SingerFragment singerFragment = SingerFragment.newInstance();
+                    return singerFragment;
+                default:
+                    return null;
+        }}
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPageAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPageAdapter.notifyDataSetChanged();
+    }
+
+}
---
+++ app/src/main/java/com/example/musicplayer/fragment/AlbumFragment.java
@@ -0,0 +1,39 @@
+package com.example.musicplayer.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.musicplayer.R;
+
+
+public class AlbumFragment extends Fragment {
+
+
+    public AlbumFragment() {
+        // Required empty public constructor
+    }
+
+    public static AlbumFragment newInstance() {
+        AlbumFragment fragment = new AlbumFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_album, container, false);
+    }
+}
